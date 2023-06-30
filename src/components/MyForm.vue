127c4148f020d26dbc36a0337a2286574a9ab137
ethsmith <ethan.smith1845@gmail.com>
2023-06-29 20:24:08 -0500

    Push initial project structure. Please excuse the warmup code

---
+++ src/components/MyForm.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const name = ref('')
+    const el = ref()
+
+    const submitForm = () => {
+      console.log(`Form submitted! Name = ${name.value}`)
+    }
+
+    onMounted(() => {
+      el.value.focus()
+    });
+
+    return {
+      el,
+      name,
+      submitForm
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <input ref="el" type="text" placeholder="name" v-model="name" />
+    <div>
+      <button @click="submitForm" :disabled="name.length == 0">Submit</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  input[type='text'] {
+    padding: 5px;
+  }
+
+  button {
+    margin-top: 20px;
+  }
+</style>
